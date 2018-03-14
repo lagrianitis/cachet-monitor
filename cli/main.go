32ac387a8345bb829e93c16e799f4887be192a65
A. Feldmann <andreas.feldmann@tausendkind.de>
2018-03-14 09:02:59 +0100

    makefile

--- cli/main.go
+++ cli/main.go
@@ -14,7 +14,7 @@ import (
 	"gopkg.in/yaml.v2"
 	"github.com/mitchellh/mapstructure"
 
-	cachet "aberfeldy/cachet-monitor"
+	cachet "github.com/aberfeldy/cachet-monitor"
 
 	docopt "github.com/docopt/docopt-go"
 
